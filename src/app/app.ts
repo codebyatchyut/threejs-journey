eb73b75aae93208461c927f867e3014e6262594e
codebyatchyut <tutikaatchyutram2003@gmail.com>
2026-09-07 22:18:33 +0530

    scroll-animation component added to app component

--- src/app/app.ts
+++ src/app/app.ts
@@ -4,11 +4,12 @@ import { RouterModule } from '@angular/router';
 import { Textures } from "./textures/textures";
 import { Lights } from './lights/lights';
 import { Galaxy } from './galaxy/galaxy';
+import { ScrollAnimation } from "./scroll-animation/scroll-animation";
 
 @Component({
   selector: 'app-root',
   standalone: true,
-  imports: [RouterModule, Cube, Textures, Lights, Galaxy],
+  imports: [RouterModule, Cube, Textures, Lights, Galaxy, ScrollAnimation],
   templateUrl: './app.html',
   styleUrl: './app.scss'
 })
